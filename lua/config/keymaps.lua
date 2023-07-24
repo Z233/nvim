3b9ee6f0248f70ac2351231a82b465c0b47c85b8
z233 <z233@msn.com>
2023-07-24 17:25:27 +0800

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -30,3 +30,6 @@ map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
 map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
 map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
 map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+
+-- ESLint: Fix all auto-fixable Problems
+map("n", "<leader>ef", '<Cmd>call VSCodeNotify("eslint.executeAutofix")<CR>', { desc = "ESLint: Fix all auto-fixable Problems" })
