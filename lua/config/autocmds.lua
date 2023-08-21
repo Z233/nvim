6e8271a512ed9c90803bfed6acefb0d099fcf6e7
z233 <z233@msn.com>
2023-08-21 15:43:27 +0800

    update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,23 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("BufEnter", {
+  callback = function()
+    vim.opt.formatoptions:remove { "c", "r", "o" }
+  end,
+  group = general,
+  desc = "Disable New Line Comment",
+})
+
+
+autocmd("TextYankPost", {
+  callback = function()
+    require("vim.highlight").on_yank { higroup = "YankHighlight", timeout = 200 }
+  end,
+  group = general,
+  desc = "Highlight when yanking",
+})
