77d2b5600f36e8530e1afbaf872104cb97df081e
z233 <z233@msn.com>
2023-12-13 16:07:29 +0800

    update

--- init.lua
+++ init.lua
@@ -1,2 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local function paste_as_plain_text(paste_mode)
+    local clipboard_content = vim.fn.getreg('+')
+    local processed_content = clipboard_content:gsub("%s+", " ")
+    vim.fn.setreg('"', processed_content)
+    
+    if paste_mode == 'P' then
+        vim.api.nvim_put({processed_content}, 'l', false, true)
+    else
+        vim.api.nvim_put({processed_content}, 'c', true, true)
+    end
+end
+
+vim.api.nvim_create_user_command('PastePlainText', function(opts)
+    paste_as_plain_text(opts.args)
+end, { nargs = '?' })
--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -8,13 +8,41 @@ return {
     },
   },
   {
-    "mg979/vim-visual-multi",
-    branch = "master",
-    vscode = true
+    'vscode-neovim/vscode-multi-cursor.nvim',
+    event = 'VeryLazy',
+    cond = not not vim.g.vscode,
+    vscode = true,
+    config = function(_, _)
+      require('vscode-multi-cursor').setup { -- Config is optional
+        -- Whether to set default mappings
+        default_mappings = true,
+        -- If set to true, only multiple cursors will be created without multiple selections
+        no_selection = false
+      }
+    end,
   },
   {
     "chrisgrieser/nvim-spider", 
     lazy = true,
     vscode = true
+  },
+  {
+    "echasnovski/mini.surround",
+    vscode = true,
+    event = "BufRead",
+    keys = function(_, keys)
+      local mappings = {
+        { "sa", desc = "Add surrounding", mode = { "n", "v" } },
+        { "sd", desc = "Delete surrounding" },
+        { "sf", desc = "Find right surrounding" },
+        { "sF", desc = "Find left surrounding" },
+        { "sh", desc = "Highlight surrounding" },
+        { "sr", desc = "Replace surrounding" },
+      }
+      return vim.list_extend(mappings, keys)
+    end,
+    config = function(_, _)
+      require("mini.surround").setup({})
+    end,
   }
 }
