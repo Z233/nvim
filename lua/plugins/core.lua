7779979e02d99699a11f7cd226e23b46c3e6fc51
Z233 <z233@msn.com>
2024-02-03 19:29:29 +0800

    update

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -8,6 +8,24 @@ return {
     },
   },
   {
+    'echasnovski/mini.ai',
+    opts = function()
+      local ai = require("mini.ai")
+      return {
+        n_lines = 500,
+        custom_textobjects = {
+          o = ai.gen_spec.treesitter({
+            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+          }, {}),
+          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+          t = false
+        },
+      }
+    end
+  },
+  {
     'vscode-neovim/vscode-multi-cursor.nvim',
     event = 'VeryLazy',
     cond = not not vim.g.vscode,
