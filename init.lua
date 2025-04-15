546327e3f74406ce2bf5a4084c0dfb4402838c5c
zhangrf <zhangrf@reolink.com.cn>
2025-04-15 17:54:19 +0800

    Update

--- init.lua
+++ init.lua
@@ -1,13 +1,14 @@
-vim.g.user_emmet_leader_key = '<C-C>'
+vim.g.user_emmet_leader_key = "<C-C>"
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
 local function parse_clipboard_to_plain_text()
-    local clipboard_content = vim.fn.getreg('+')
-    local processed_content = clipboard_content:match("^%s*(.-)%s*$")
-    vim.fn.setreg('*', processed_content)
-    vim.fn.setreg('+', processed_content)
+  local clipboard_content = vim.fn.getreg("+")
+  local processed_content = clipboard_content:match("^%s*(.-)%s*$")
+  vim.fn.setreg("*", processed_content)
+  vim.fn.setreg("+", processed_content)
 end
 
-vim.api.nvim_create_user_command('ParseClipboardToPlainText', parse_clipboard_to_plain_text, {})
+vim.api.nvim_create_user_command("ParseClipboardToPlainText", parse_clipboard_to_plain_text, {})
+
