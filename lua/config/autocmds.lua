6396db2ee40ea5af63adf18238c14b6b3586723f
z233 <z233@msn.com>
2025-08-29 16:41:47 +0800

    Update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,21 +3,4 @@
 -- Add any additional autocmds here
 
 
-local autocmd = vim.api.nvim_create_autocmd
-
-autocmd("BufEnter", {
-  callback = function()
-    vim.opt.formatoptions:remove { "c", "r", "o" }
-  end,
-  group = general,
-  desc = "Disable New Line Comment",
-})
-
-
-autocmd("TextYankPost", {
-  callback = function()
-    require("vim.highlight").on_yank { higroup = "YankHighlight", timeout = 200 }
-  end,
-  group = general,
-  desc = "Highlight when yanking",
-})
+-- local autocmd = vim.api.nvim_create_autocmd
