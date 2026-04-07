67ad509cfa688a3cc07686af186c14e16877520f
z233 <z233@msn.com>
2026-04-07 14:38:11 +0800

    feat: rewrite init.lua and options.lua without LazyVim

--- init.lua
+++ init.lua
@@ -1,14 +1,12 @@
-vim.g.user_emmet_leader_key = "<C-C>"
+-- ~/.config/nvim/init.lua
 
--- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.options")
 require("config.lazy")
 
-local function parse_clipboard_to_plain_text()
-  local clipboard_content = vim.fn.getreg("+")
-  local processed_content = clipboard_content:match("^%s*(.-)%s*$")
-  vim.fn.setreg("*", processed_content)
-  vim.fn.setreg("+", processed_content)
-end
-
-vim.api.nvim_create_user_command("ParseClipboardToPlainText", parse_clipboard_to_plain_text, {})
-
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    require("config.keymaps")
+    require("config.autocmds")
+  end,
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,17 +1,47 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- ~/.config/nvim/lua/config/options.lua
 
-vim.opt.guifont = "Dank Mono:h15"
-vim.opt.clipboard = "unnamedplus"
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.g.user_emmet_leader_key = "<C-C>"
 
+-- Encoding
 vim.cmd("language en_US.UTF-8")
 
-if not vim.g.vscode then
-  vim.opt.relativenumber = true
-  vim.opt.scrolloff = 8
-  vim.opt.sidescrolloff = 8
-  vim.opt.undofile = true
-  vim.opt.wrap = false
-  vim.opt.smoothscroll = true
-end
+-- Appearance
+vim.opt.termguicolors = true
+vim.opt.guifont = "Dank Mono:h15"
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Indentation
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.smartindent = true
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Splits
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Scrolling
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.smoothscroll = true
+
+-- Editing
+vim.opt.clipboard = "unnamedplus"
+vim.opt.undofile = true
+vim.opt.wrap = false
+vim.opt.mouse = "a"
+
+-- Performance
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
