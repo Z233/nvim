a90ea7982f6b5a752a0b7d6fa7a2e5de6ad411ae
z233 <z233@msn.com>
2026-04-07 11:37:21 +0800

    fix: switch jieba.vim to rust branch and build lua51 binding for nvim

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -264,8 +264,8 @@ return {
   },
   {
     "kkew3/jieba.vim",
-    tag = "v1.0.5",
-    build = "./build.sh",
+    branch = "rust",
+    build = "JIEBA_VIM_INSTALL_NVIM=1 ./build.sh",
     init = function()
       vim.g.jieba_vim_lazy = 1
       vim.g.jieba_vim_keymap = 1
