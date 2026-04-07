69c336496412a46a14695421ee591870d0a1eef8
z233 <z233@msn.com>
2026-04-07 15:05:46 +0800

    feat: rewrite autocmds with yank ring, highlight, close-with-q, cursor restore

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,11 +1,9 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
-
 local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
 
--- Yank history: rotate yanks into numbered registers (simple yank ring)
+-- Yank history: rotate yanks into numbered registers
 autocmd("TextYankPost", {
+  group = augroup("yank_history", { clear = true }),
   callback = function()
     local op = vim.v.event.operator
     if op == "y" or op == "d" or op == "c" then
@@ -14,5 +12,47 @@ autocmd("TextYankPost", {
       end
       vim.fn.setreg("1", vim.fn.getreg('"'))
     end
+  end,
+})
+
+-- Highlight on yank
+autocmd("TextYankPost", {
+  group = augroup("highlight_yank", { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+-- Close special buffers with q
+autocmd("FileType", {
+  group = augroup("close_with_q", { clear = true }),
+  pattern = { "help", "man", "qf", "checkhealth", "notify", "lspinfo", "startuptime" },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+-- Restore cursor position on file open
+autocmd("BufReadPost", {
+  group = augroup("restore_cursor", { clear = true }),
+  callback = function(event)
+    local mark = vim.api.nvim_buf_get_mark(event.buf, '"')
+    local line_count = vim.api.nvim_buf_line_count(event.buf)
+    if mark[1] > 0 and mark[1] <= line_count then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- Auto-create parent directories on save
+autocmd("BufWritePre", {
+  group = augroup("auto_create_dir", { clear = true }),
+  callback = function(event)
+    if event.match:match("^%w%w+:[\\/][\\/]") then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
   end,
 })
