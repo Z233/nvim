edfc54283133e47159d1e601625961f936049860
Z233 <z233@msn.com>
2026-08-15 13:41:39 +0800

    fix(colorscheme): use white Tokyo Night surfaces

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,6 +7,15 @@ return {
     opts = {
       style = "day",
       terminal_colors = true,
+      on_colors = function(colors)
+        local white = "#ffffff"
+
+        colors.bg = white
+        colors.bg_sidebar = white
+        colors.bg_float = white
+        colors.bg_popup = white
+        colors.bg_statusline = white
+      end,
     },
     config = function(_, opts)
       require("tokyonight").setup(opts)
