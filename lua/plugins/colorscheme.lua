5b39a6a002b6f31d386312dd032a80ed99964977
Z233 <z233@msn.com>
2026-08-17 11:25:19 +0800

    fix(colorscheme): preserve terminal colors and white surfaces

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,7 +6,19 @@ return {
     cond = not vim.g.vscode,
     config = function()
       vim.o.background = "light"
-      require("token").setup()
+      require("token").setup({
+        terminal_colors = true,
+        on_colors = function(colors)
+          local white = "#ffffff"
+
+          colors.bg0 = white
+          colors.bg1 = white
+          colors.bg2 = white
+          colors.bg3 = white
+          colors.bg4 = white
+          colors.bg5 = white
+        end,
+      })
       vim.cmd.colorscheme("token-temper")
     end,
   },
