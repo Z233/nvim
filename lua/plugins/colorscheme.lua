c5404c9312db57366795c9dd3b0b4a0d373208ac
Z233 <z233@msn.com>
2026-08-19 14:46:00 +0800

    fix(colorscheme): remove transparent background, follow theme color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,18 +5,6 @@ return {
     priority = 1000,
     cond = not vim.g.vscode,
     config = function()
-      require("Eva-Theme").setup({
-        override_highlight = function(variant, palette)
-          return {
-            Normal = { bg = "NONE" },
-            NormalNC = { bg = "NONE" },
-            SignColumn = { bg = "NONE" },
-            EndOfBuffer = { bg = "NONE" },
-            WinBar = { bg = "NONE" },
-            WinBarNC = { bg = "NONE" },
-          }
-        end,
-      })
       vim.cmd.colorscheme("Eva-Light")
     end,
   },
