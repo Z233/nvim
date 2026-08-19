190bd6791a8e956441ffb7e23feddedc183773a1
Z233 <z233@msn.com>
2026-08-19 14:39:26 +0800

    feat(colorscheme): switch to Eva-Theme Light with transparent background

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,23 @@
 return {
   {
-    "folke/tokyonight.nvim",
+    "sharpchen/Eva-Theme.nvim",
     lazy = false,
     priority = 1000,
     cond = not vim.g.vscode,
-    opts = {
-      style = "day",
-      terminal_colors = true,
-    },
-    config = function(_, opts)
-      require("tokyonight").setup(opts)
-      vim.cmd.colorscheme("tokyonight-day")
+    config = function()
+      require("Eva-Theme").setup({
+        override_highlight = function(variant, palette)
+          return {
+            Normal = { bg = "NONE" },
+            NormalNC = { bg = "NONE" },
+            SignColumn = { bg = "NONE" },
+            EndOfBuffer = { bg = "NONE" },
+            WinBar = { bg = "NONE" },
+            WinBarNC = { bg = "NONE" },
+          }
+        end,
+      })
+      vim.cmd.colorscheme("Eva-Light")
     end,
   },
 }
