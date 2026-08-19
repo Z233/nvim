e07c26d3f1edca42b81fc03d34954b45acee9d7e
Z233 <z233@msn.com>
2026-08-19 14:33:03 +0800

    revert(colorscheme): switch back to Tokyo Night Day

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,30 +1,16 @@
 return {
   {
-    "ThorstenRhau/token",
+    "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     cond = not vim.g.vscode,
-    config = function()
-      vim.o.background = "light"
-      require("token").setup({
-        terminal_colors = true,
-        plugins = {
-          blink = true,
-          diffview = true,
-          fzf = true,
-          gitsigns = true,
-          lazy = true,
-          mason = true,
-          neo_tree = true,
-          noice = true,
-          whichkey = true,
-        },
-        on_highlights = function(highlights)
-          highlights.Normal.bg = "NONE"
-          highlights.NormalNC.bg = "NONE"
-        end,
-      })
-      vim.cmd.colorscheme("token-temper")
+    opts = {
+      style = "day",
+      terminal_colors = true,
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+      vim.cmd.colorscheme("tokyonight-day")
     end,
   },
 }
