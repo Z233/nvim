110f895a4c136477118aab4f70ecbc64d6d0a140
Z233 <z233@msn.com>
2026-08-22 22:56:35 +0800

    fix(treesitter): use current installed parser API

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -21,7 +21,7 @@ return {
       }
 
       -- Only install missing parsers
-      local installed = require("nvim-treesitter").get_installed()
+      local installed = require("nvim-treesitter.info").installed_parsers()
       local installed_set = {}
       for _, p in ipairs(installed) do
         installed_set[p] = true
