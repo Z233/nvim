8af9b29afa83f8f4c31e8d0fe642364b419c39db
Z233 <z233@msn.com>
2026-08-22 17:10:01 +0800

    feat(fuzzy): route LSP multi-location jumps through fzf-lua

--- lua/plugins/fuzzy.lua
+++ lua/plugins/fuzzy.lua
@@ -3,6 +3,9 @@ return {
     "ibhagwan/fzf-lua",
     cond = not vim.g.vscode,
     cmd = "FzfLua",
+    -- Load before the first LSP jump so ui_select hooks gI/gd/gy multi-result
+    -- picks (nvim default opens a quickfix window that Esc cannot close).
+    event = { "LspAttach" },
     keys = {
       { "<leader>ff", "<cmd>FzfLua files<cr>", desc = "Find Files" },
       { "<leader>fg", "<cmd>FzfLua live_grep<cr>", desc = "Live Grep" },
@@ -14,6 +17,8 @@ return {
       { "<leader>/", "<cmd>FzfLua grep_curbuf<cr>", desc = "Grep Current Buffer" },
     },
     opts = {
+      -- Route vim.ui.select (LSP multi-location jumps, code actions) through fzf
+      ui_select = true,
       defaults = {
         git_icons = false,
         file_icons = false,
